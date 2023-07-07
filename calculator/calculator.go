905f6c5d548e294c92e6b5b33dc8c107ac5d918a
iragsraghu <raghavendra@frontier.xyz>
2023-07-07 12:43:03 +0530

    sum, sub, multipy and division function added

---
+++ calculator/calculator.go
@@ -0,0 +1,17 @@
+package calculator
+
+func sum(a, b int64) int64 {
+	return a + b
+}
+
+func sub(a, b int64) int64 {
+	return a - b
+}
+
+func multipy(a, b int64) int64 {
+	return a * b
+}
+
+func division(a, b int64) int64 {
+	return a / b
+}
