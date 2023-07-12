8d6cc94a657d0346068e24c7b267fcd04a5f4e2b
iragsraghu <raghavendra@frontier.xyz>
2023-07-12 13:49:55 +0530

    slices and map

---
+++ slices/slices.go
@@ -0,0 +1,20 @@
+package slices
+
+import "fmt"
+
+func DataStructureWithLoop() {
+	tables := []int{2, 4, 6, 8, 10, 12, 14, 16, 18, 20}
+	for k, v := range tables {
+		fmt.Printf("Index %v \t Value %v", k, v)
+	}
+}
+
+func DataStructureWithMap() {
+	personInfo := map[string]int{
+		"Raghavendra": 30,
+		"PIN Code":    582101,
+	}
+	for k, v := range personInfo {
+		fmt.Printf("Index %v \t Value %v", k, v)
+	}
+}
