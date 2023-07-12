0c884e1425d082384b5732f373d8c5fdc4319869
iragsraghu <raghavendra@frontier.xyz>
2023-07-12 14:00:41 +0530

    slices and map

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -1,5 +1,7 @@
 package calculator
 
+import "fmt"
+
 func Sum(a, b int64) int64 {
 	return a + b
 }
@@ -14,4 +16,21 @@ func Multipy(a, b int64) int64 {
 
 func Division(a, b int64) int64 {
 	return a / b
+}
+
+func DataStructureWithLoop() {
+	tables := []int{2, 4, 6, 8, 10, 12, 14, 16, 18, 20}
+	for k, v := range tables {
+		fmt.Printf("Index %v \t Value %v", k, v)
+	}
+}
+
+func DataStructureWithMap() {
+	personInfo := map[string]int{
+		"Raghavendra": 30,
+		"PIN Code":    582101,
+	}
+	for k, v := range personInfo {
+		fmt.Printf("Index %v \t Value %v", k, v)
+	}
 }
